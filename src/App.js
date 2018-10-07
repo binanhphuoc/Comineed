ef072e81af080fdd2a81a9ee494621dd8954d104
thantienduc <duc.than@mavs.uta.edu>
2018-10-06 20:10:31 -0500

    add inforpanel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar'
+import Inforpanel from './components/Infopanel/inforpanel'
+import { Button } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
         <iframe
           align="left"
           frameborder={0} style={{width:"50vw", height: "100vh",borderLeft:0}}
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAmRGqEXBVcufLBcFGDw1T-Q5f7WxzUMj4&q=Space+Needle,Seattle+WA" allowfullscreen>
         </iframe>
-        Hello Kitty
+          <Inforpanel title='Tuan Hung'/>
+
       </div>
     );
   }
---
+++ src/components/Infopanel/components/Titlebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function SimpleAppBar(props) {
+  const { classes, title } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar align="right"
+          frameborder={0} style={{width:"50vw", height: "10vh",borderLeft:0,backgroundColor:"white"}}>
+        <Toolbar>
+          <Typography variant="title" color="black">
+          {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+SimpleAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleAppBar);
